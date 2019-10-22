9c128cb74d6057cbb334dc2c0429a5fd84295ffe
Keven <kmax136@gmail.com>
2019-10-22 01:20:59 -0300

    Adding redux and architecture flux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Routes from './routes';
+import {Provider} from 'react-redux';
+import store from './store';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
---
+++ src/store/ducks/alert.js
@@ -0,0 +1,32 @@
+import {createActions, createReducer} from 'reduxsauce';
+
+export const {Types, Creators} = createActions({
+  addAlert: ['show', 'title', 'message'],
+  removeAlert: [''],
+});
+
+console.log(alert);
+
+const INITIAL_STATE = {
+  show: '',
+  title: '',
+  message: '',
+};
+
+const add = (state = INITIAL_STATE, action) => ({
+  ...state,
+  show: action.show,
+  title: action.title,
+  message: action.message,
+});
+
+const remove = (state = INITIAL_STATE, action) => ({
+  show: false,
+  title: '',
+  message: '',
+});
+
+export default createReducer(INITIAL_STATE, {
+  [Types.ADD_ALERT]: add,
+  [Types.REMOVE_ALERT]: remove,
+});
