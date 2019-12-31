c4d64bc943876540542fbd86812c40a1556f7217
Keven <kmax136@gmail.com>
2019-12-31 01:17:53 -0300

    Refactoring with eslint

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-community/async-storage'
+import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://localhost:3000',
-});
+})
 
 api.interceptors.request.use(async config => {
   config.headers = {
     Authorization: `Bearer ${await AsyncStorage.getItem('token')}`,
-  };
-  return config;
-});
-export default api;
+  }
+  return config
+})
+export default api
