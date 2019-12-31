dfba2f6549428c90c477ca4ba061d500ea84ab22
Keven <kmax136@gmail.com>
2019-12-31 01:12:29 -0300

    Config eslint e prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'eslint:recommended', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'import-helpers'],
+  rules: {
+    'prettier/prettier': ['error'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': 'warn',
+    'import-helpers/order-imports': [
+      'error',
+      {
+        newlinesBetween: 'always',
+        groups: ['/^react/', 'module', '/^//', ['sibling', '/^style/']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+}
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto'
+}
